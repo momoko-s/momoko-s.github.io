4dbbf47ddf629f4f2c84dc2fd6d08ddc2141d0f5
momoko-s <shigashigarune@gmail.com>
2019-02-02 15:47:45 +0900

    [sample_site]sassをコンパイルすところまで

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,55 @@
-// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
-const path = require('path');
+// [定数] webpack の出力オプションを指定します
+// 'production' か 'development' を指定
+const MODE = 'development';
+
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = (MODE === 'development');
 
 module.exports = {
-  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-  mode: 'development',
-  // エントリーポイントの設定
-  entry: './src/js/app.js',
-  // 出力の設定
-  output: {
-    // 出力するファイル名
-    filename: 'bundle.js',
-    // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
-  }
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: MODE,
+
+  module: {
+    rules: [
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        // ローダー名
+        use: [
+          // linkタグに出力する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドを取り込む
+              url: true,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            },
+          },
+          // Sassをバンドルするための機能
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
+      {
+        // 対象となるファイルの拡張子
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        // 画像をBase64として取り込む
+        loader: 'url-loader'
+      }
+    ],
+  },
 };
